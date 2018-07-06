8658d1ad255e889f414efe6e854fd0a381c3d3e8
JoeyLiu <cliu0571@gmail.com>
2018-07-06 01:27:06 -0400

    init class 05 heap and graph

---
+++ HeapAndGraphSearch/src/com/myPractice/class05/HeapAndGraphSearch/Main.java
@@ -0,0 +1,10 @@
+package com.myPractice.class05.HeapAndGraphSearch;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+        System.out.println("Class 05 Heap and Graph Search...");
+    }
+}
