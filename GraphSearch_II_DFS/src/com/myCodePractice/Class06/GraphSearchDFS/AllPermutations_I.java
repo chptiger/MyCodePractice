189bdcdb237b8da279154f60bfe28a0128189da2
JoeyLiu <cliu0571@gmail.com>
2018-07-08 02:14:56 -0400

    check in coins and permutations

--- GraphSearch_II_DFS/src/com/myCodePractice/Class06/GraphSearchDFS/AllPermutations_I.java
+++ GraphSearch_II_DFS/src/com/myCodePractice/Class06/GraphSearchDFS/AllPermutations_I.java
@@ -9,11 +9,60 @@
 
 package com.myCodePractice.Class06.GraphSearchDFS;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AllPermutations_I {
     public List<String> permutations(String set) {
         // Write your solution here.
-        return null;
+        List<String> result = new ArrayList<String>();
+        char[] array = set.toCharArray();
+        helper(array, 0, result);
+        return result;
+    }
+
+    // solution 1: DFS with swapping
+    // choose the character to be at the position of "index"
+    // all the already chosen positions are (0, index-1)
+    // all the candidate characters can be at position "index"
+    // are in the subarray of (index, array.length-1)
+    private void helper(char[] array, int index, List<String> result) {
+        // terminate conditions:
+        // only when we have already chosen the characters for all position,
+        // we can have a complete permutation
+        System.out.println("index->" + index + " " + new String(array));
+
+        if (index == array.length - 1) {
+            result.add(new String(array));
+            return;
+        }
+
+        // all the possible characters could be placed at index are
+        // the characters in the subarray (index, array.length-1)
+        for (int i = index; i < array.length; i++) {
+            swap(array, index, i);
+            helper(array, index + 1, result);
+            swap(array, index, i);
+        }
+    }
+
+    private void swap(char[] array, int left, int right) {
+        if (left == right) {
+            return;
+        } else if (array[left] == array[right]) {
+            return;
+        }
+        char temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+
+
+    @Test
+    public void testAllPermutations() {
+        System.out.println(permutations("abc"));
     }
 }
--- GraphSearch_II_DFS/src/com/myCodePractice/Class06/GraphSearchDFS/CombinationsOfCoins.java
+++ GraphSearch_II_DFS/src/com/myCodePractice/Class06/GraphSearchDFS/CombinationsOfCoins.java
@@ -30,11 +30,44 @@
 
 package com.myCodePractice.Class06.GraphSearchDFS;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CombinationsOfCoins {
     public List<List<Integer>> combinations(int target, int[] coins) {
         // Write your solution here.
+        List<List<Integer>> result = new ArrayList<List<Integer>>(); // All result stores in a list of list<integer>
+        List<Integer> cur = new ArrayList<Integer>(); // Cur presents each combination, cur.get(i) = number of coins[i]
+
+        helper(target, coins, 0, cur, result);
         return null;
+    }
+
+    // target: remaining cents we need to complete
+    // coins: all the possible different coins
+    // index: we want to see how many coins we need for coins[index]
+    private void helper(int target, int[] coins, int index, List<Integer> cur, List<List<Integer>> result) {
+        // terminate conditions:
+        // done at index == coins.length where all coins have been picked
+        if (index == coins.length - 1) {
+            if (target % coins[coins.length - 1] == 0) {
+                cur.add(target / coins[coins.length - 1]);
+                result.add(new ArrayList<>(cur));
+                cur.remove(cur.size() - 1);
+            }
+            return;
+        }
+
+        // for coins[index], we can pick 0,1,2,..., target / coins[index] coins
+        int max = target / coins[index];
+        for (int i = 0; i <= max; i++) {
+            cur.add(i);
+            // remember to modify the remaining cents for the next level
+            helper(target - i * coins[index], coins, index + 1, cur, result);
+            cur.remove(cur.size()-1);
+        }
     }
 }
