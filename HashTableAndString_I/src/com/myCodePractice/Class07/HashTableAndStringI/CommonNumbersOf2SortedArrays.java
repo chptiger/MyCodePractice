6eab21a15b96506910e7bc073048d69006a2d602
liujoe <joey.liu@avon.com>
2018-07-11 17:36:50 -0400

    Check in test method

--- HashTableAndString_I/src/com/myCodePractice/Class07/HashTableAndStringI/CommonNumbersOf2SortedArrays.java
+++ HashTableAndString_I/src/com/myCodePractice/Class07/HashTableAndStringI/CommonNumbersOf2SortedArrays.java
@@ -10,11 +10,75 @@
 //        A = {1, 1, 2, 2, 3}, B = {1, 1, 2, 5, 6}, common numbers are [1, 1, 2]
 package com.myCodePractice.Class07.HashTableAndStringI;
 
-import java.util.List;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
 
 public class CommonNumbersOf2SortedArrays {
+    // Method 1: 2 pointers
     public List<Integer> common(List<Integer> A, List<Integer> B) {
         // Write your solution here
-        return null;
+        // Assumption: A and B are both not null
+        int i = 0, j = 0;
+        List<Integer> common = new ArrayList<>();
+        while (i<A.size() && j < B.size()){
+            if (A.get(i).equals(B.get(j))){
+                common.add(A.get(i));
+                i++;
+                j++;
+            }else if (A.get(i)<B.get(j)){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        return common;
+    }
+
+    // method 2: use HashMap
+    public List<Integer> common_Map(List<Integer> A, List<Integer> B){
+        List<Integer> common = new ArrayList<>();
+        HashMap<Integer, Integer> countA = new HashMap<>();
+        for (Integer num : A){
+            Integer ct = A.get(num);
+            if (ct != null){
+                countA.put(num, ct+1);
+            }else{
+                countA.put(num, 1);
+            }
+        }
+        HashMap<Integer, Integer> countB = new HashMap<>();
+        for (Integer num : B){
+            System.out.println("B : "+num);
+            Integer ct = B.get(num);
+            if (ct != null){
+                countB.put(num, ct+1);
+            }else{
+                countB.put(num, 1);
+            }
+        }
+        System.out.println();
+        for(Map.Entry<Integer, Integer> entry : countA.entrySet()){
+            Integer ctInB = countB.get(entry.getKey());
+            if (ctInB!=null){
+                int appear = Math.min(entry.getValue(), ctInB);
+                for (int i = 0;i<appear;i++){
+                    common.add(ctInB);
+                }
+            }
+        }
+        return common;
+    }
+
+    @Test
+    public void testCommon(){
+        List<Integer> a = Arrays.asList(1,2,3,1,4);
+        List<Integer> b = Arrays.asList(2,3,1,1,0);
+        //System.out.println(common(a,b));
+        System.out.println(common_Map(a,b));
+//        Assert.assertEquals(Arrays.asList(1,1,2,3), common(a,b));
+//        Assert.assertEquals(Arrays.asList(1,1,2,3), common_Map(a,b));
     }
 }
--- HashTableAndString_I/src/com/myCodePractice/Class07/HashTableAndStringI/MissingNumberI.java
+++ HashTableAndString_I/src/com/myCodePractice/Class07/HashTableAndStringI/MissingNumberI.java
@@ -1,5 +1,6 @@
 //Description
-//        Given an integer array of size N - 1, containing all the numbers from 1 to N except one, find the missing number.
+//        Given an integer array of size N - 1, containing all the numbers from 1 to N except one,
+//        find the missing number.
 //
 //        Assumptions
 //
@@ -11,9 +12,109 @@
 //        A = {}, the missing number is 1
 package com.myCodePractice.Class07.HashTableAndStringI;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+
 public class MissingNumberI {
-    public int missing(int[] array) {
+    // Method 1 Set
+    public int missing_bySet(int[] array) {
         // Write your solution here
-        return 0;
+        // Corner case
+        if (array == null){
+            return -1;
+        }
+        if (array.length == 0){
+            return 0;
+        }
+
+        int n = array.length + 1;
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : array){
+            set.add(num);
+        }
+        for (int i = 1;i<n;i++){
+            if (!set.contains(i)){
+                return i;
+            }
+        }
+        return n;
+    }
+    // Method 2: use sum
+    public int missing_bySum(int[] array){
+        if (array == null){
+            return -1;
+        }
+        if (array.length == 0){
+            return 0;
+        }
+        int n = array.length+1;
+        long targetSum = n*(n+1)/2;
+        long actualSum = 0;
+        for (int num: array){
+            actualSum += num;
+        }
+        return (int)(targetSum - actualSum);
+    }
+
+    // method 3: bit opeartion
+    public int missing_byBit(int[] array){
+        if (array == null){
+            return -1;
+        }
+        if (array.length == 0){
+            return 0;
+        }
+        int n = array.length +1;
+        int xor = 0;
+        for (int i = 1;i<=n;i++){
+            xor ^= i;
+        }
+        for (int num:array){
+            xor ^= num;
+        }
+        return xor;
+    }
+
+    // method 4: swap the num to its corresponding position
+    public int missing_bySwap(int[] array){
+        if (array == null){
+            return -1;
+        }
+        if (array.length == 0){
+            return 0;
+        }
+        for (int i = 0; i<array.length;i++){
+            while (array[i] != i+1 && array[i] != array.length +1){
+                swap(array, i , array[i] -1);
+            }
+        }
+
+        for (int i = 0;i < array.length;i++){
+            if (array[i] != i+1){
+                return i+1;
+            }
+        }
+        return array.length +1;
+    }
+    private void swap(int[] array, int i,int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    @Test
+    public void testMissing(){
+        int[] array = {1,2,4,5};
+        Assert.assertEquals(3, missing_bySet(array));
+        Assert.assertEquals(3, missing_bySum(array));
+        Assert.assertEquals(3, missing_byBit(array));
+        Assert.assertEquals(3, missing_bySwap(array));
+        int[] array2 = {1,2,3,4,5};
+        Assert.assertEquals(6, missing_bySet(array2));
+        Assert.assertEquals(6, missing_bySum(array2));
+        Assert.assertEquals(6, missing_byBit(array2));
+        Assert.assertEquals(6, missing_bySwap(array2));
     }
 }
