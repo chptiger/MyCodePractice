83b6c520c3af6bd28adff43100128b634227e643
JoeyLiu <cliu0571@gmail.com>
2018-07-15 19:44:17 -0400

    check in longest substring

---
+++ StringII/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+`
+0src/com/myCodePractice/Class08/ReorderArray.java,8/9/899c8a8dbe2f7ac7a755702a0d726f82f4593343
+_
+/src/com/myCodePractice/Class08/AllAnagrams.java,e/9/e99a6e0edd3a82a64bcedd6e33903e00253fb9a1
+e
+5src/com/myCodePractice/Class08/AllPermutationsII.java,f/0/f099bee85d2e5c8001f2c7b5e1a25ada1f14f102
+f
+6src/com/myCodePractice/Class08/DecompressStringII.java,8/c/8ccfc375a45143114ae56924fa7fe981c67997e0
--- StringII/src/com/myCodePractice/Class08/LongestSubstringWithoutRepeatingCharacters.java
+++ StringII/src/com/myCodePractice/Class08/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,11 +1,41 @@
 package com.myCodePractice.Class08;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
 //Description
-//        Given a string, find the longest substring without any repeating characters and return the length of it. The input string is guaranteed to be not null.
+//        Given a string, find the longest substring without any repeating characters and return the length of it.
+// The input string is guaranteed to be not null.
 //
 //        For example, the longest substring without repeating letters for "bcdfbd" is "bcdf", we should return 4 in this case.
 public class LongestSubstringWithoutRepeatingCharacters {
     public int longest(String input) {
         // Write your solution here
-        return 0;
+        // assumption: input string is not null
+        // the distinct set contains all distinct characters
+        // in the sliding window of (slow, fast)
+        Set<Character> distinct = new HashSet<>();
+        int slow = 0, fast = 0;
+        int longest = 0;
+        while (fast < input.length()) {
+            if (distinct.contains(input.charAt(fast))) {
+                // if there is duplicate char, we need to move the slow pointer
+                distinct.remove(input.charAt(slow++));
+            } else {
+                // if there is no duplicate char, we can slide fast pointer
+                // and we have a new sliding window of (slow, fast) containing all distinct characters
+                distinct.add(input.charAt(fast++));
+                longest = Math.max(longest, fast - slow);
+            }
+        }
+        return longest;
+    }
+
+    @Test
+    public void test_Longest(){
+        Assert.assertEquals(4, longest("bcdfbd"));
     }
 }
