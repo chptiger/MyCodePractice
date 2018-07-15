b08e07647d9fa78be95049bfea89d534efcfb8b7
JoeyLiu <cliu0571@gmail.com>
2018-07-14 22:07:39 -0400

    check in right shift

--- StringII/src/com/myCodePractice/Class08/RightShiftByNChar.java
+++ StringII/src/com/myCodePractice/Class08/RightShiftByNChar.java
@@ -1,4 +1,8 @@
 package com.myCodePractice.Class08;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 //Right shift a given string by n characters.
 //
 //        Assumptions
@@ -11,6 +15,33 @@ package com.myCodePractice.Class08;
 public class RightShiftByNChar {
     public String rightShift(String input, int n) {
         // Write your solution here
-        return null;
+        // assumption : input != null, n >= 0
+        if (input.length() <= 1) {
+            return input;
+        }
+
+        char[] array = input.toCharArray();
+        n %= array.length;
+        reverse(array, array.length - n, array.length - 1);
+        reverse(array, 0, array.length - n - 1);
+        reverse(array, 0, array.length - 1);
+        return new String(array);
+    }
+
+    private void reverse(char[] array, int left, int right) {
+        while (left < right) {
+            char temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    @Test
+    public void test_rightShift() {
+        Assert.assertEquals("efgabcd", rightShift("abcdefg", 3));
+        Assert.assertEquals("a", rightShift("a", 3));
+
     }
 }
