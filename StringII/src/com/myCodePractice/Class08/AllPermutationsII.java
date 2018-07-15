344d97fff7fa6cb258ab5e435432295a8d24a4d3
JoeyLiu <cliu0571@gmail.com>
2018-07-15 01:42:05 -0400

    check in all permutations II

--- StringII/src/com/myCodePractice/Class08/AllPermutationsII.java
+++ StringII/src/com/myCodePractice/Class08/AllPermutationsII.java
@@ -10,11 +10,59 @@
 //        Depth First Search
 package com.myCodePractice.Class08;
 
-import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
 
+import java.util.*;
+
+// all permutations with duplicate characters
 public class AllPermutationsII {
     public List<String> permutations(String set) {
         // Write your solution here.
-        return null;
+        List<String> result = new ArrayList<>();
+        if (set == null) {
+            return result;
+        }
+        char[] array = set.toCharArray();
+        helper(array, 0, result);
+        return result;
+    }
+
+    // index: at the level of index, we are to determined for the current permutation,
+    // which element is positioned at the index
+    private void helper(char[] array, int index, List<String> result) {
+        // Base case: if we have already determined for the current permutation,
+        // which element is positioned at the index
+        if (index == array.length) {
+            result.add(new String(array));
+            return;
+        }
+        // Notice: the rule is just for the current level,
+        // if a certain element is picked,
+        // we cannot pick any of its duplicates.
+        // so that we use a set to record all the distinct elements
+        Set<Character> used = new HashSet<>();
+        for (int i = index; i < array.length; i++) {
+            // user.add(array[i]) will return false if the value of array[i] is already in set
+            if (used.add(array[i])) {
+                swap(array, i, index);
+                // go to next level, index + 1
+                helper(array, index + 1, result);
+                // don't forget to do the clear operation when backtracking
+                swap(array, i, index);
+            }
+        }
+    }
+
+    private void swap(char[] array, int left, int right) {
+        char temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+
+    @Test
+    public void test_allPermutations() {
+        Assert.assertEquals(new ArrayList<>(Arrays.asList("aba", "aab", "baa")), permutations("aba"));
+        Assert.assertEquals(new ArrayList<>(Arrays.asList("abc", "acb", "bac","bca","cba","cab")), permutations("abc"));
     }
 }
