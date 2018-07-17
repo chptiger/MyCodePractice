f4867f3189f3d684606d9fe6db53c838a14de989
JoeyLiu <cliu0571@gmail.com>
2018-07-17 01:54:51 -0400

    check in class 10 nQueens method 1

--- RecursionII/src/com/myCodePractice/Class10/NQueens.java
+++ RecursionII/src/com/myCodePractice/Class10/NQueens.java
@@ -29,13 +29,51 @@ import java.util.List;
         Backtracking
         Depth First Search*/
 public class NQueens {
+    // method 1, validate the queen position in O(n) each time
     public List<List<Integer>> nqueens(int n) {
         // Write your solution here.
-        return null;
+        // Assumption: n > 0
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        // cur will be a list of size n, and cur[i] is the column number
+        // where the queen on row i positioned.
+        List<Integer> cur = new ArrayList<>();
+        helper(n, cur, result);
+        return result;
+    }
+
+    private void helper(int n, List<Integer> cur, List<List<Integer>> result) {
+        // base case: when for all the rows we know where the queen is positioned
+        if (cur.size() == n) {
+            result.add(new ArrayList<>(cur));
+        }
+        for (int i = 0; i < n; i++) {
+            // check if putting a queen at column i at current row is valid
+            if (valid(cur, i)) {
+                cur.add(i);
+                helper(n, cur, result);
+                cur.remove(cur.size() - 1);
+            }
+        }
+    }
+
+    private boolean valid(List<Integer> cur, int column) {
+        int row = cur.size();
+        for (int i = 0; i < row; i++) {
+            if (cur.get(i) == column || Math.abs(cur.get(i) - column) == row -i){
+                return false;
+            }
+        }
+        return true;
     }
 
     @Test
-    public void test_NQueens(){
+    public void test_NQueens() {
+        for (List<Integer> list : nqueens(4)){
+            for (Integer i:list){
+                System.out.print("->"+i);
+            }
+            System.out.println();
+        }
         //Assert.assertEquals(new ArrayList(Arrays.asList()));
     }
 }
