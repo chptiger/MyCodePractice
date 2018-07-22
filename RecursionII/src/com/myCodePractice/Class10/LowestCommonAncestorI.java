e5143a6f5a1fe578941113659e308df893d4324d
JoeyLiu <cliu0571@gmail.com>
2018-07-22 01:31:26 -0400

    check in class 10 LCA

--- RecursionII/src/com/myCodePractice/Class10/LowestCommonAncestorI.java
+++ RecursionII/src/com/myCodePractice/Class10/LowestCommonAncestorI.java
@@ -17,6 +17,7 @@ package com.myCodePractice.Class10;
         Medium
         Binary Tree
 */
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -34,11 +35,26 @@ public class LowestCommonAncestorI {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode one, TreeNode two) {
         // Write your solution here.
-        return root;
+        if (root == null || root == one || root == two) {
+            return root;
+        }
+        TreeNode ll = lowestCommonAncestor(root.left, one, two);
+        TreeNode lr = lowestCommonAncestor(root.right, one, two);
+        if (ll != null && lr != null) {
+            return root;
+        }
+        return ll == null ? lr : ll;
     }
 
     @Test
-    public void test_LCA(){
-        //Assert.assertEquals("", lowestCommonAncestor());
+    public void test_LCA() {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(12);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(14);
+        Assert.assertEquals(5, lowestCommonAncestor(root, root.left.left, root.right.right).key);
+        Assert.assertEquals(9, lowestCommonAncestor(root, root.left.left, root.left).key);
     }
 }
