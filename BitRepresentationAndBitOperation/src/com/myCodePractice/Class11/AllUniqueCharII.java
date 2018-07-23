804723e240359f9a53bfff1d8281f248265d69b1
cliu0571 <cliu0571@gmail.com>
2018-07-23 16:15:29 -0400

    Update AllUniqueCharII.java

--- BitRepresentationAndBitOperation/src/com/myCodePractice/Class11/AllUniqueCharII.java
+++ BitRepresentationAndBitOperation/src/com/myCodePractice/Class11/AllUniqueCharII.java
@@ -1,4 +1,18 @@
 package com.myCodePractice.Class11;
+/*Description
+Determine if the characters of a given string are all unique.
 
+Assumptions
+
+We are using ASCII charset, the value of valid characters are from 0 to 255
+The given string is not null
+Examples
+
+all the characters in "abA+\8" are unique
+"abA+\a88" contains duplicate characters*/
 public class AllUniqueCharII {
+  public boolean allUnique(String word) {
+    // Write your solution here
+    return false;
+  }
 }
