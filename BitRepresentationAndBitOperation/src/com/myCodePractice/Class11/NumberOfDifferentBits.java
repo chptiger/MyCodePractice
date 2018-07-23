43816d1674f8ed1e51c993d90af17dfb32c7f311
cliu0571 <cliu0571@gmail.com>
2018-07-23 16:13:27 -0400

    Update NumberOfDifferentBits.java

--- BitRepresentationAndBitOperation/src/com/myCodePractice/Class11/NumberOfDifferentBits.java
+++ BitRepresentationAndBitOperation/src/com/myCodePractice/Class11/NumberOfDifferentBits.java
@@ -1,4 +1,13 @@
 package com.myCodePractice.Class11;
+/*Description
+Determine the number of bits that are different for two given integers.
 
+Examples
+
+5(“0101”) and 8(“1000”) has 3 different bits*/
 public class NumberOfDifferentBits {
+  public int diffBits(int a, int b) {
+    // Write your solution here
+    return -1;
+  }
 }
