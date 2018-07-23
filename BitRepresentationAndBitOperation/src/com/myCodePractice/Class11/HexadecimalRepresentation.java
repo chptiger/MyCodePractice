e3a7ac620dc42c2d42e8de9baf1f1a51c8905bff
cliu0571 <cliu0571@gmail.com>
2018-07-23 16:14:14 -0400

    Update HexadecimalRepresentation.java

--- BitRepresentationAndBitOperation/src/com/myCodePractice/Class11/HexadecimalRepresentation.java
+++ BitRepresentationAndBitOperation/src/com/myCodePractice/Class11/HexadecimalRepresentation.java
@@ -1,4 +1,16 @@
 package com.myCodePractice.Class11;
+/*Description
+Generate the hexadecimal representation for a given non-negative integer number as a string. The hex representation should start with "0x".
 
+There should not be extra zeros on the left side.
+
+Examples
+
+0's hex representation is "0x0"
+255's hex representation is "0xFF"*/
 public class HexadecimalRepresentation {
+  public String hex(int number) {
+    // Write your solution here
+    return null;
+  }
 }
