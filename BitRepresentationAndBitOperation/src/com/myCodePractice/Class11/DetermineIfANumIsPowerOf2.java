f6f44a75d30f1267fd3023a0577af5d44dbe6c3c
cliu0571 <cliu0571@gmail.com>
2018-07-23 16:12:44 -0400

    Update DetermineIfANumIsPowerOf2.java

--- BitRepresentationAndBitOperation/src/com/myCodePractice/Class11/DetermineIfANumIsPowerOf2.java
+++ BitRepresentationAndBitOperation/src/com/myCodePractice/Class11/DetermineIfANumIsPowerOf2.java
@@ -1,4 +1,16 @@
 package com.myCodePractice.Class11;
+/*Description
+Determine if a given integer is power of 2.
 
+Examples
+
+16 is power of 2 (2 ^ 4)
+3 is not
+0 is not
+-1 is not*/
 public class DetermineIfANumIsPowerOf2 {
+  public boolean isPowerOfTwo(int number) {
+    // Write your solution here
+    return false;
+  }
 }
