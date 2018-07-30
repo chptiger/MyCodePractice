5d56ea2d02a31f761c71da212a6d3bf0ef5cfee2
cliu0571 <cliu0571@gmail.com>
2018-07-29 23:46:36 -0400

    Update DictionaryWord.java

--- DynamicProgrammingII/src/com/myCodePractice/Class13/DictionaryWord.java
+++ DynamicProgrammingII/src/com/myCodePractice/Class13/DictionaryWord.java
@@ -1,4 +1,22 @@
 package com.myCodePractice.Class13;
+/*
+Description
+Given a word and a dictionary, determine if it can be composed by concatenating words from the given dictionary.
 
+Assumptions
+
+The given word is not null and is not empty
+The given dictionary is not null and is not empty and all the words in the dictionary are not null or empty
+Examples
+
+Dictionary: {“bob”, “cat”, “rob”}
+
+Word: “robob” return false
+
+Word: “robcatbob” return true since it can be composed by "rob", "cat", "bob"
+*/
 public class DictionaryWord {
+  public boolean canBreak(String input, String[] dict) {
+    // Write your solution here
+  }
 }
