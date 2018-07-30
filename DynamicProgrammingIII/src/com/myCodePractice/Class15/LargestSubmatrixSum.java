2be991c3929d64893de824102c6f321e6843ab07
cliu0571 <cliu0571@gmail.com>
2018-07-29 23:54:16 -0400

    Update LargestSubmatrixSum.java

--- DynamicProgrammingIII/src/com/myCodePractice/Class15/LargestSubmatrixSum.java
+++ DynamicProgrammingIII/src/com/myCodePractice/Class15/LargestSubmatrixSum.java
@@ -1,4 +1,27 @@
 package com.myCodePractice.Class15;
+/*
+Description
+Given a matrix that contains integers, find the submatrix with the largest sum.
 
+Return the sum of the submatrix.
+
+Assumptions
+
+The given matrix is not null and has size of M * N, where M >= 1 and N >= 1
+Examples
+
+{ {1, -2, -1, 4},
+
+  {1, -1,  1, 1},
+
+  {0, -1, -1, 1},
+
+  {0,  0,  1, 1} }
+
+the largest submatrix su
+*/
 public class LargestSubmatrixSum {
+  public int largest(int[][] matrix) {
+    // Write your solution here
+  }
 }
