e5657a8f78e7f9b9d7c1393baacc91b46064d9be
cliu0571 <cliu0571@gmail.com>
2018-07-29 23:49:25 -0400

    Update LongestConsecutive1s.java

--- DynamicProgrammingIII/src/com/myCodePractice/Class15/LongestConsecutive1s.java
+++ DynamicProgrammingIII/src/com/myCodePractice/Class15/LongestConsecutive1s.java
@@ -1,4 +1,17 @@
 package com.myCodePractice.Class15;
+/*
+Description
+Given an array containing only 0s and 1s, find the length of the longest subarray of consecutive 1s.
 
+Assumptions
+
+The given array is not null
+Examples
+
+{0, 1, 0, 1, 1, 1, 0}, the longest consecutive 1s is 3.
+*/
 public class LongestConsecutive1s {
+  public int longest(int[] array) {
+    // Write your solution here
+  }
 }
