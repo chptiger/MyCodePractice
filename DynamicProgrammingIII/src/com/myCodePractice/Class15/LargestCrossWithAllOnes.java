3d05d524cda9857af544887f8b9a38be40ef827d
cliu0571 <cliu0571@gmail.com>
2018-07-29 23:49:56 -0400

    Update LargestCrossWithAllOnes.java

--- DynamicProgrammingIII/src/com/myCodePractice/Class15/LargestCrossWithAllOnes.java
+++ DynamicProgrammingIII/src/com/myCodePractice/Class15/LargestCrossWithAllOnes.java
@@ -1,4 +1,27 @@
 package com.myCodePractice.Class15;
+/*
+Description
+Given a matrix that contains only 1s and 0s, find the largest cross which contains only 1s, with the same arm lengths and the four arms joining at the central point.
 
+Return the arm length of the largest cross.
+
+Assumptions
+
+The given matrix is not null, has size of N * M, N >= 0 and M >= 0.
+Examples
+
+{ {0, 0, 0, 0},
+
+  {1, 1, 1, 1},
+
+  {0, 1, 1, 1},
+
+  {1, 0, 1, 1} }
+
+the largest cross of 1s has arm length 2.
+*/
 public class LargestCrossWithAllOnes {
+  public int largest(int[][] matrix) {
+    // Write your solution here
+  }
 }
