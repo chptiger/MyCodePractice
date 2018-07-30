d970751a77e105eb6fa65fa96dcbf98e610fc37f
cliu0571 <cliu0571@gmail.com>
2018-07-29 23:55:04 -0400

    Update LargestXWithAllOnes.java

--- DynamicProgrammingIII/src/com/myCodePractice/Class15/LargestXWithAllOnes.java
+++ DynamicProgrammingIII/src/com/myCodePractice/Class15/LargestXWithAllOnes.java
@@ -1,4 +1,27 @@
 package com.myCodePractice.Class15;
+/*
+Description
+Given a matrix that contains only 1s and 0s, find the largest X shape which contains only 1s, with the same arm lengths and the four arms joining at the central point.
 
+Return the arm length of the largest X shape.
+
+Assumptions
+
+The given matrix is not null, has size of N * M, N >= 0 and M >= 0.
+Examples
+
+{ {0, 0, 0, 0},
+
+  {1, 1, 1, 1},
+
+  {0, 1, 1, 1},
+
+  {1, 0, 1, 1} }
+
+the largest X of 1s has arm length 2.
+*/
 public class LargestXWithAllOnes {
+  public int largest(int[][] matrix) {
+    // Write your solution here
+  }
 }
