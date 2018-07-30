2da62f92288ec9d19769696fed8aa6992ac87eb3
cliu0571 <cliu0571@gmail.com>
2018-07-29 23:44:56 -0400

    Update LongestAscendingSubarray.java

--- DynamicProgrammingI/src/com/myCodePractice/Class12/LongestAscendingSubarray.java
+++ DynamicProgrammingI/src/com/myCodePractice/Class12/LongestAscendingSubarray.java
@@ -1,4 +1,19 @@
 package com.myCodePractice.Class12;
+/*
+Description
+Given an unsorted array, find the length of the longest subarray in which the numbers are in ascending order.
 
+Assumptions
+
+The given array is not null
+Examples
+
+{7, 2, 3, 1, 5, 8, 9, 6}, longest ascending subarray is {1, 5, 8, 9}, length is 4.
+
+{1, 2, 3, 3, 4, 4, 5}, longest ascending subarray is {1, 2, 3}, length is 3.
+*/
 public class LongestAscendingSubarray {
+  public int longest(int[] array) {
+    // Write your solution here
+  }
 }
