24d7c190c459c4571d27918e8b7065abdba24b61
cliu0571 <cliu0571@gmail.com>
2018-07-29 23:44:29 -0400

    Update ArrayHopperI.java

--- DynamicProgrammingI/src/com/myCodePractice/Class12/ArrayHopperI.java
+++ DynamicProgrammingI/src/com/myCodePractice/Class12/ArrayHopperI.java
@@ -1,4 +1,19 @@
 package com.myCodePractice.Class12;
+/*
+Description
+Given an array A of non-negative integers, you are initially positioned at index 0 of the array. A[i] means the maximum jump distance from that position (you can only jump towards the end of the array). Determine if you are able to reach the last index.
 
+Assumptions
+
+The given array is not null and has length of at least 1.
+Examples
+
+{1, 3, 2, 0, 3}, we are able to reach the end of array(jump to index 1 then reach the end of the array)
+
+{2, 1, 1, 0, 2}, we are not able to reach the end of array
+*/
 public class ArrayHopperI {
+  public boolean canJump(int[] array) {
+    // Write your solution here
+  }
 }
