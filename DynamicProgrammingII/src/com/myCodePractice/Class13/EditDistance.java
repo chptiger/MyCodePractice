6b79bb125f40e69fe498aeeba2339d7a8c61b67a
cliu0571 <cliu0571@gmail.com>
2018-07-29 23:47:00 -0400

    Update EditDistance.java

--- DynamicProgrammingII/src/com/myCodePractice/Class13/EditDistance.java
+++ DynamicProgrammingII/src/com/myCodePractice/Class13/EditDistance.java
@@ -1,4 +1,19 @@
 package com.myCodePractice.Class13;
+/*
+Description
+Given two strings of alphanumeric characters, determine the minimum number of Replace, Delete, and Insert operations needed to transform one string into the other.
 
+Assumptions
+
+Both strings are not null
+Examples
+
+string one: “sigh”, string two : “asith”
+
+the edit distance between one and two is 2 (one insert “a” at front then replace “g” with “t”).
+*/
 public class EditDistance {
+  public int editDistance(String one, String two) {
+    // Write your solution here
+  }
 }
