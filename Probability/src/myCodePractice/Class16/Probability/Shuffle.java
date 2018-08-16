dc00172f6b3fac83a772b111370646ed2539ad2c
JoeyLiu <cliu0571@gmail.com>
2018-08-16 00:26:22 -0400

    init class 16 shuffle

--- Probability/src/myCodePractice/Class16/Probability/Shuffle.java
+++ Probability/src/myCodePractice/Class16/Probability/Shuffle.java
@@ -1,4 +1,9 @@
 package myCodePractice.Class16.Probability;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /*Description
         Given an array of integers (without any duplicates), shuffle the array such that all permutations are equally likely to be generated.
 
@@ -8,5 +13,28 @@ package myCodePractice.Class16.Probability;
 public class Shuffle {
     public void shuffle(int[] array) {
         // Write your solution here.
+        // assumption: array is not null
+        if (array.length <= 1) {
+            return;
+        }
+        // from right to left, for index i-1, randomly pick one of the first i elements
+        for (int i = array.length; i >= 1; i--) {
+            int index = (int) (Math.random() * i);
+            swap(array, i - 1, index);
+        }
     }
+
+    private void swap(int[] array, int left, int right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+
+    @Test
+    public void test_shuffle() {
+        int[] array = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        shuffle(array);
+        System.out.println(Arrays.toString(array));
+    }
+
 }
